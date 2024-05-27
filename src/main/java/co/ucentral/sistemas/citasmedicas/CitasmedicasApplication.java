a8e30f0f0ed9e641bab02ad3197ddd4d2db3bb3b
Santiago Aguirre <santiago_guayara@hotmail.com>
2024-05-26 22:06:33 -0500

    coreccion de SonarLint

--- src/main/java/co/ucentral/sistemas/citasmedicas/CitasmedicasApplication.java
+++ src/main/java/co/ucentral/sistemas/citasmedicas/CitasmedicasApplication.java
@@ -151,7 +151,7 @@ public class CitasmedicasApplication implements CommandLineRunner {
 		List<Cita> citas = new ArrayList<>();
 
 		// Definir la hora de inicio de las citas
-		LocalDateTime horaInicio = LocalDateTime.parse("2024-05-15 08:00:00", formatter);
+		LocalDateTime horaInicio = LocalDateTime.parse("2024-05-29 08:00:00", formatter);
 
 		// Crear 10 citas con 20 minutos de diferencia entre cada una
 		for (int i = 0; i < 20; i++) {
